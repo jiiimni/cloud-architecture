704c518b27d36cb12fa28b4a9cd14e144d27aa0c
jiiimni <leejimin8951@naver.com>
2026-03-13 09:53:16 +0900

    fix: 테스트 환경에서 S3 빈 생성 오류 수정

--- src/test/java/kr/spartaclub/cloudarchitecture/CloudArchitectureApplicationTests.java
+++ src/test/java/kr/spartaclub/cloudarchitecture/CloudArchitectureApplicationTests.java
@@ -2,12 +2,13 @@ package kr.spartaclub.cloudarchitecture;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CloudArchitectureApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
