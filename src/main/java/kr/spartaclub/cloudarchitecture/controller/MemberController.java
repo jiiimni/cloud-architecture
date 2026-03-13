cc4e40ed980f5671e0de894d608a649d8c2bb841
jiiimni <leejimin8951@naver.com>
2026-03-13 10:45:10 +0900

    test: ci/cd 자동 배포 확인

--- src/main/java/kr/spartaclub/cloudarchitecture/controller/MemberController.java
+++ src/main/java/kr/spartaclub/cloudarchitecture/controller/MemberController.java
@@ -26,6 +26,7 @@ public class MemberController {
 
     @GetMapping("/{id}")
     public MemberResponse getMember(@PathVariable Long id) {
+        System.out.println("deploy test");
         return memberService.getMember(id);
     }
 
